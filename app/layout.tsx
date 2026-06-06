8a30006bbddce2137e91b10e355fb45077b85c77
Huynh Anh Kiet <akiethuynh0904@gmail.com>
2026-06-06 23:57:49 +0700

    Add reset Css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import {GlobalStyle} from "./resetCss/resetAllCss";
+import StyledComponentsRegistry from "./lib/registry";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <StyledComponentsRegistry>
+        <GlobalStyle />
+          <body>
+            {children}
+          </body>
+      </StyledComponentsRegistry>
     </html>
   );
 }
